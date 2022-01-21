bd2571cbdf4a34a8a503a2163e9f2ad508faa1ab
Wesley de Groot <wes@vista.aero>
2022-01-21 14:31:34 +0100

    first commit

---
+++ Appsterdam/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Appsterdam
+//
+//  Created by Wesley de Groot on 21/01/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
