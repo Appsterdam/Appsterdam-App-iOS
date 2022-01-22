78e502daf5bdb53dcc9ccce157ae994dfdafcf38
Wesley de Groot <wes@vista.aero>
2022-01-22 12:21:55 +0100

    Added MainView if we go all SwiftUI, fixed the 'placeholder' autolayout issues

---
+++ Appsterdam/MainView.swift
@@ -0,0 +1,35 @@
+//
+//  MainView.swift
+//  Appsterdam
+//
+//  Created by Wesley de Groot on 22/01/2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+
+            EventView()
+                .tabItem {
+                    Label("Events", systemImage: "person.3.fill")
+                }
+
+            AboutView()
+                .tabItem {
+                    Label("About", systemImage: "info.circle.fill")
+                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ Appsterdam/SwiftUI VC/EventView.swift
@@ -0,0 +1,20 @@
+//
+//  EventView.swift
+//  Appsterdam
+//
+//  Created by Wesley de Groot on 22/01/2022.
+//
+
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventView()
+    }
+}
