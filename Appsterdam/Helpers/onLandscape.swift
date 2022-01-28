e2fc626067b1acbd90012cfb271b5442553f451f
Wesley de Groot <wes@vista.aero>
2022-01-28 19:35:12 +0100

    Add onLandscape helper func

--- Appsterdam/Helpers/onLandscape.swift
+++ Appsterdam/Helpers/onLandscape.swift
@@ -7,10 +7,21 @@
 
 import SwiftUI
 
-// A View wrapper to make the modifier easier to use
+/// A view wrapper for
 extension View {
-    @ViewBuilder
-    func onLandscape<Transform: View>(transform: (Self) -> Transform) -> some View {
+    /// Perform an action only if the device is landscape mode
+    ///
+    /// Only perform a view modifier if the device is in landscape mode.
+    /// Use this view modifier after your element. e.g.
+    ///
+    ///     Text("I will be hidden, if we are in landscape mode")
+    ///       .onLandscape {
+    ///         // Hide if we are in landscape mode
+    ///         $0.hidden()
+    ///       }
+    ///
+    /// - Returns: ViewModifier
+    @ViewBuilder public func onLandscape<Transform: View>(transform: (Self) -> Transform) -> some View {
         if UIScreen.main.traitCollection.verticalSizeClass == .compact {
             transform(self)
         }
@@ -19,8 +30,19 @@ extension View {
         }
     }
 
-    @ViewBuilder
-    func onPortrait<Transform: View>(transform: (Self) -> Transform) -> some View {
+    /// Perform an action only if the device is portrait mode
+    ///
+    /// Only perform a view modifier if the device is in portrait mode.
+    /// Use this view modifier after your element. e.g.
+    ///
+    ///     Text("I will be hidden, if we are in portrait mode")
+    ///       .onPortrait {
+    ///         // Hide if we are in portrait mode
+    ///         $0.hidden()
+    ///       }
+    ///
+    /// - Returns: ViewModifier
+    @ViewBuilder public func onPortrait<Transform: View>(transform: (Self) -> Transform) -> some View {
         if UIScreen.main.traitCollection.verticalSizeClass == .regular {
             transform(self)
         }
