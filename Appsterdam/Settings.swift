6456e462d000cb108c641677823fc2fec0798ef9
Wesley de Groot <wes@vista.aero>
2022-02-13 19:47:58 +0100

    Added option to hide search field, Fixes #11

--- Appsterdam/Settings.swift
+++ Appsterdam/Settings.swift
@@ -12,27 +12,30 @@ class Settings {
     /// Static variable settings
     public static let shared = Settings()
 
-    @UserDefault("showSectionIndexTitles", default: false)
-    /// Show section index titles
-    var showSectionIndexTitles: Bool
-
-    @UserDefault("stats.appruns", default: 0)
+    // MARK: APP Stats/settings
+    @UserDefault("app.appruns", default: 0)
     /// how many times the app is openend?
     var appRuns: Int
 
-
+    // MARK: About settings
     @UserDefault("about.openInApp", default: true)
     /// Open about links in app (safari)?
     var aboutOpenInApp: Bool
 
+    // MARK: Events
     @UserDefault("events.openInApp", default: true)
     /// Open events in app (safari)
     var eventsOpenInApp: Bool
+
+    @UserDefault("events.hideSearch", default: true)
+    /// Hide search in events
+    var eventsHideSearch: Bool
 
     @UserDefault("events.description", default: true)
     /// Show event description?
     var eventsDescription: Bool
 
+    // MARK: Appsterdam User Account
     @Keychain(item: "appsterdam.username")
     var appsterdam_username: String?
 
