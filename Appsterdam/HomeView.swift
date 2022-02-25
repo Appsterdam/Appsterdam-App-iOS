b93e0a9fedd29dacdc12ce8062eeb283e0c90479
Wesley de Groot <wes@vista.aero>
2022-02-25 14:04:57 +0100

    Added splash screen, fixes #10

--- Appsterdam/HomeView.swift
+++ Appsterdam/HomeView.swift
@@ -15,46 +15,43 @@ struct HomeView: View {
     // initial URL string
     @State private var urlString = "https://appsterdam.rs"
 
+    @State var animate = false
+    @State var showSplash = true
+
+    @UserDefault("experiment.splash", default: true)
+    var experimentalSplashscreen: Bool
+
     var body: some View {
-        ScrollView {
-            VStack {
-                VStack(alignment: .trailing) {
-                    Spacer().frame(maxWidth:.infinity)
-                Button {
-                    UIApplication.shared.openAppSettings()
-                } label: {
-                    Image(systemName: "gear")
-                        .font(.system(.largeTitle))
-                        .frame(width: 25, height: 25)
-                }.padding(.trailing, 25)
-                }.padding(.bottom, -25)
+        ZStack {
+            ZStack {
+                ScrollView {
+                    VStack {
+                        VStack(alignment: .trailing) {
+                            Spacer().frame(maxWidth:.infinity)
+                            Button {
+                                UIApplication.shared.openAppSettings()
+                            } label: {
+                                Image(systemName: "gear")
+                                    .font(.system(.title2))
+                            }.padding(.trailing, 25)
+                        }.padding(.bottom, -25)
 
-                Image(
-                    "Appsterdam_logo",
-                    bundle: nil,
-                    label: Text("Appsterdam Logo")
-                )
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .padding()
+                        Image(
+                            "Appsterdam_logo",
+                            bundle: nil,
+                            label: Text("Appsterdam Logo")
+                        )
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                            .padding()
 
-                Text("Appsterdam")
-                    .font(.largeTitle)
-                    .padding()
+                        Text("Appsterdam")
+                            .font(.largeTitle)
+                            .padding()
 
-                if Bundle.main.isInTestFlight {
-                    Text("""
-                    ***TESTFLIGHT BUILD***
-                    Please test:
-                    - Events route
-                    - Events general (landscape, portrait)
-                    - Links/buttons in about
-                    - Change Settings press (gear icon)
-                    """)
-                }
-                Text(
-                    .init("""
+                        Text(
+                            .init("""
                 **[Join the community](https://appsterdam.rs/join-community/)**
                 We are 4,500 members of makers, designers and developers.
                 App makers helping app makers -  come join our community!
@@ -68,18 +65,47 @@ struct HomeView: View {
                 Our members are always developing new projects, products and solutions.
                 Check out the projects they are most proud of here.
                 """))
-                    .frame(
-                        maxWidth: .infinity,
-                        alignment: .leading
-                    )
-                    .padding(.leading, 5)
+                            .frame(
+                                maxWidth: .infinity,
+                                alignment: .leading
+                            )
+                            .padding(.leading, 5)
+                    }
+                }
+                // Safari
+                .sheet(isPresented: $showSafari,
+                       content: {
+                    SafariView(url: $urlString)
+                })
+            }
+
+            // Animation
+            ZStack {
+                Color.red
+                Image("Appsterdam_logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .scaleEffect(animate ? 1000 : 1)
+                // Animate the scale effect
+                    .animation(Animation.easeIn(duration: 1))
+            }
+            .edgesIgnoringSafeArea(.all)
+            .opacity(showSplash ? 1 : 0)
+            // Animate the opacity to 0
+            .animation(Animation.easeOut(duration: 1.5))
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    if experimentalSplashscreen {
+                        Aurora.shared.log("Enabled Experimental, splash")
+                        animate.toggle()
+                    }
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    showSplash.toggle()
+                }
             }
         }
-        // Safari
-        .sheet(isPresented: $showSafari,
-               content: {
-            SafariView(url: $urlString)
-        })
     }
 }
 
